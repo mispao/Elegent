eb1c8a5f338853fcfa72cc50ce0015a019d9a456
paopao <1316345688@qq.com>
2023-11-09 17:53:18 +0800

    first commit

---
+++ recycleview/app/src/main/java/cn/itcast/recycleview/weather.java
@@ -0,0 +1,16 @@
+package cn.itcast.recycleview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class weather extends AppCompatActivity {
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.weather);
+
+    }
+
+}
